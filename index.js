7ecd7443f48d7160a662c1e3655be1f22f29a799
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-05-21 23:20:57 +0900

    Remove peerDependencies using shared babel-core.

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
 'use strict';
 
-require('./patch');
-var babel = require('babel-core');
-var t = babel.types;
-var Transformer = babel.Transformer;
-var helper = require('babel-rtts-helper')('assert');
+var patch = require('./patch');
+var createHelper = require('babel-rtts-helper');
 
-module.exports = new Transformer('angular2-annotations', {
-  ClassDeclaration: function ClassDeclaration(node, parent) {
-    var classRef = node.id;
-    var classBody = node.body.body;
+module.exports = function (babel) {
+  patch(babel);
+  var t = babel.types;
+  var Transformer = babel.Transformer;
+  var helper = createHelper(babel, 'assert');
 
-    // Create additional statements for parameter decorators and types.
-    var decorators;
-    var types;
-    classBody.forEach(function (bodyNode) {
-      if (bodyNode.type === 'MethodDefinition' && bodyNode.kind === 'constructor') {
-        decorators = parameterDecorators(bodyNode.value.params, classRef);
-        types = parameterTypes(bodyNode.value.params, classRef);
+  return new Transformer('angular2-annotations', {
+    ClassDeclaration: function ClassDeclaration(node, parent) {
+      var classRef = node.id;
+      var classBody = node.body.body;
+
+      // Create additional statements for parameter decorators and types.
+      var decorators;
+      var types;
+      classBody.forEach(function (bodyNode) {
+        if (bodyNode.type === 'MethodDefinition' && bodyNode.kind === 'constructor') {
+          decorators = parameterDecorators(bodyNode.value.params, classRef);
+          types = parameterTypes(bodyNode.value.params, classRef);
+        }
+      });
+      var additionalStatements = [].concat(decorators, types).filter(Boolean);
+
+      // If not found, do nothing.
+      if (additionalStatements.length === 0) {
+        return undefined;
       }
+
+      // Append additional statements to program.
+      if (parent.type === 'ExportNamedDeclaration' || parent.type === 'ExportDefaultDeclaration') {
+        // The class declaration is wrapped by an export declaration.
+        this.parentPath.replaceWithMultiple([parent].concat(additionalStatements));
+      } else {
+        return [node].concat(additionalStatements);
+      }
+    }
+  });
+
+  // Returns an array of parameter decorator call statements for a class.
+  function parameterDecorators(params, classRef) {
+    var decoratorLists = params.map(function (param, i) {
+      var decorators = param.decorators;
+      if (!decorators) {
+        return [];
+      }
+      // Delete parameter decorators because they are invalid in vanilla babel.
+      // They might be just ignored though.
+      param.decorators = null;
+
+      return decorators.map(function (decorator) {
+        var call = decorator.expression;
+        var args = [classRef, t.identifier('null'), t.identifier(i)];
+        return t.expressionStatement(t.callExpression(call, args));
+      });
     });
-    var additionalStatements = [].concat(decorators, types).filter(Boolean);
-
-    // If not found, do nothing.
-    if (additionalStatements.length === 0) {
-      return undefined;
-    }
-
-    // Append additional statements to program.
-    if (parent.type === 'ExportNamedDeclaration' || parent.type === 'ExportDefaultDeclaration') {
-      // The class declaration is wrapped by an export declaration.
-      this.parentPath.replaceWithMultiple([parent].concat(additionalStatements));
-    } else {
-      return [node].concat(additionalStatements);
-    }
+    // Flatten.
+    return Array.prototype.concat.apply([], decoratorLists);
   }
-});
 
-// Returns an array of parameter decorator call statements for a class.
-function parameterDecorators(params, classRef) {
-  var decoratorLists = params.map(function (param, i) {
-    var decorators = param.decorators;
-    if (!decorators) {
+  // Returns an array of define 'parameters' metadata statements for a class.
+  // The array may contain zero or one statements.
+  function parameterTypes(params, classRef) {
+    var types = params.map(function (param) {
+      var annotation = param.typeAnnotation && param.typeAnnotation.typeAnnotation;
+      if (!annotation) {
+        return null;
+      }
+      return helper.typeForAnnotation(annotation);
+    });
+    if (!types.some(Boolean)) {
       return [];
     }
-    // Delete parameter decorators because they are invalid in vanilla babel.
-    // They might be just ignored though.
-    param.decorators = null;
-
-    return decorators.map(function (decorator) {
-      var call = decorator.expression;
-      var args = [classRef, t.identifier('null'), t.identifier(i)];
-      return t.expressionStatement(t.callExpression(call, args));
-    });
-  });
-  // Flatten.
-  return Array.prototype.concat.apply([], decoratorLists);
-}
-
-// Returns an array of define 'parameters' metadata statements for a class.
-// The array may contain zero or one statements.
-function parameterTypes(params, classRef) {
-  var types = params.map(function (param) {
-    var annotation = param.typeAnnotation && param.typeAnnotation.typeAnnotation;
-    if (!annotation) {
-      return null;
-    }
-    return helper.typeForAnnotation(annotation);
-  });
-  if (!types.some(Boolean)) {
-    return [];
+    return [defineMetadata('design:paramtypes', t.arrayExpression(types), classRef)];
   }
-  return [defineMetadata('design:paramtypes', t.arrayExpression(types), classRef)];
-}
 
-// Returns an AST for define metadata statement.
-function defineMetadata(key, value, target) {
-  return t.expressionStatement(t.callExpression(
-    t.memberExpression(t.identifier('Reflect'), t.identifier('defineMetadata')),
-    [t.literal(key), value, target]
-  ));
-}
+  // Returns an AST for define metadata statement.
+  function defineMetadata(key, value, target) {
+    return t.expressionStatement(t.callExpression(
+      t.memberExpression(t.identifier('Reflect'), t.identifier('defineMetadata')),
+      [t.literal(key), value, target]
+    ));
+  }
+};
--- patch.js
+++ patch.js
@@ -1,42 +1,43 @@
 'use strict';
 
-var babel = require('babel-core');
-var tt = babel.acorn.tokTypes;
+module.exports = function (babel) {
+  var tt = babel.acorn.tokTypes;
 
-// HACK: Monkey patching to parse parameter decorators. Based on babel-core 5.1.10.
-babel.acorn.Parser.prototype.parseBindingList = function (close, allowEmpty, allowTrailingComma) {
-  var elts = [],
-      first = true;
-  while (!this.eat(close)) {
-    if (first) {
-      first = false;
-    } else {
-      this.expect(tt.comma);
-    }
-    if (allowEmpty && this.type === tt.comma) {
-      elts.push(null);
-    } else if (allowTrailingComma && this.afterTrailingComma(close)) {
-      break;
-    } else if (this.type === tt.ellipsis) {
-      elts.push(this.parseAssignableListItemTypes(this.parseRest()));
-      this.expect(close);
-      break;
-    } else {
-      // Start monkey patching
-      var decorators = [];
-      while (this.type === tt.at) {
-        decorators.push(this.parseDecorator());
+  // HACK: Monkey patching to parse parameter decorators. Based on babel-core 5.1.10.
+  babel.acorn.Parser.prototype.parseBindingList = function (close, allowEmpty, allowTrailingComma) {
+    var elts = [],
+        first = true;
+    while (!this.eat(close)) {
+      if (first) {
+        first = false;
+      } else {
+        this.expect(tt.comma);
       }
-      // End monkey patching
-      var left = this.parseMaybeDefault();
-      // Start monkey patching
-      if (decorators.length > 0) {
-        left.decorators = decorators;
+      if (allowEmpty && this.type === tt.comma) {
+        elts.push(null);
+      } else if (allowTrailingComma && this.afterTrailingComma(close)) {
+        break;
+      } else if (this.type === tt.ellipsis) {
+        elts.push(this.parseAssignableListItemTypes(this.parseRest()));
+        this.expect(close);
+        break;
+      } else {
+        // Start monkey patching
+        var decorators = [];
+        while (this.type === tt.at) {
+          decorators.push(this.parseDecorator());
+        }
+        // End monkey patching
+        var left = this.parseMaybeDefault();
+        // Start monkey patching
+        if (decorators.length > 0) {
+          left.decorators = decorators;
+        }
+        // End monkey patching
+        this.parseAssignableListItemTypes(left);
+        elts.push(this.parseMaybeDefault(null, left));
       }
-      // End monkey patching
-      this.parseAssignableListItemTypes(left);
-      elts.push(this.parseMaybeDefault(null, left));
     }
-  }
-  return elts;
+    return elts;
+  };
 };
